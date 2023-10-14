1f2aabe54c3943bd8b1ca64a9cb6fd5eb1351be4
NicholasStambaugh <nastambaugh@gmail.com>
2023-10-14 16:28:32 -0400

    updated header js to typescript :D

--- src/assets/js/header.ts
+++ src/assets/js/header.ts
@@ -1,5 +1,5 @@
 window.addEventListener("scroll", function () {
-    const header = document.getElementById("header");
+    const header = document.getElementById("header") as HTMLElement;
     if (window.scrollY > 0.0001) {
       header.classList.add("hidden");
     } else {
