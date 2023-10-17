a712e6a3f56b7124970a384e04aeca482f372a2e
NicholasStambaugh <nastambaugh@gmail.com>
2023-10-17 12:32:21 -0400

    continued authentication work

--- client/server/src/index.ts
+++ client/server/src/index.ts
@@ -3,10 +3,13 @@ import mongoose from 'mongoose';
 import ProfileModel from "./models/profile";
 import { config } from 'dotenv';
 import cors from 'cors';
+import jwt from 'jsonwebtoken';
+import authenticateToken from '../../server/middleware/index';
+import JWT
 // import CompanyModel from "./models/company";
 
 config();
-
+authenticateToken
 const PORT = 5000;
 const app = express();
 
@@ -15,12 +18,12 @@ app.use(express.json());
 
 
 //profiles (login)
-app.get('/profile',async (req: Request, res: Response) => {
+app.get('/profile',authenticateToken,async (req: Request, res: Response) => {
     const profiles = await ProfileModel.find();
     res.json(profiles);
 });
 
-app.post('/profile', async (req: Request, res: Response) => {
+app.post('/profile',authenticateToken,async (req: Request, res: Response) => {
     console.log(req.body);
 
     const newProfile = new ProfileModel({
@@ -37,6 +40,20 @@ app.delete('/profile/:profileId', async (req: Request, res: Response) => {
            const deletedprofile = await ProfileModel.findByIdAndDelete(profileId);
             res.json(deletedprofile);
 });
+
+app.post('/login', async (req: Request, res: Response) => {
+    // Check the username and password in your database.
+    // If they are valid, generate a JWT token.
+    const { username, password } = req.body;
+    if (isValidUser(username, password)) {
+      const user = { username: username };
+      const token = jwt.sign(user, process.env.JWT_SECRET);
+      res.json({ token });
+    } else {
+      res.status(401).send('Authentication failed.');
+    }
+  });
+  
 
 //company profiles (login)
 // app.get('/company',async (req: Request, res: Response) => {
